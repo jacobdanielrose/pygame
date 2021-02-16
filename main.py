3785532138d01e5ff7242f251dac61c02aa1664f
Jacob Rose <jacobdanielrose@gmail.com>
2021-02-16 17:16:56 +0100

    Basic window with white background

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game!")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+WHITE = (255, 255, 255)
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+        WIN.fill(WHITE)
+        pygame.display.update()
+
+    pygame.quit()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()

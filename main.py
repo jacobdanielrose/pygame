c628b9f272ef15e405bbc4d6ea2ff7e77d4055bc
Jacob Rose <jacobdanielrose@gmail.com>
2021-02-16 17:26:00 +0100

    Imported asset and displayed spaceship to screen.

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game!")
 
 WHITE = (255, 255, 255)
+FPS = 60
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+
+
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    pygame.display.update()
 
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                
-        WIN.fill(WHITE)
-        pygame.display.update()
+        draw_window()
 
     pygame.quit()
 

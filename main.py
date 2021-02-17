75e343d3c4b85961127bf704474133df608c09dd
Jacob Rose <jacobdanielrose@gmail.com>
2021-02-17 15:47:51 +0100

    wrote movement functions

--- main.py
+++ main.py
@@ -15,10 +15,8 @@ YELLOW = (255, 255, 0)
 
 BORDER = pygame.Rect(WIDTH // 2 - 5, 0, 10, HEIGHT)
 
-
 BULLET_HIT_SOUND = pygame.mixer.Sound('Assets/Grenade+1.mp3')
 BULLET_FIRE_SOUND = pygame.mixer.Sound('Assets/Gun+Silencer.mp3')
-
 
 HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
 WINNER_FONT = pygame.font.SysFont('comicsans', 100)
@@ -50,17 +48,39 @@ def draw_window(red, yellow, red_bullets, yellow_bullets, red_health, yellow_hea
     yellow_health_text = HEALTH_FONT.render("Health: " + str(yellow_health), True, WHITE)
     WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
     WIN.blit(yellow_health_text, (10, 10))
-    
+
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
-    WIN.blit(RED_SPACESHIP, (red.x, red.y))\
-    
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+
     for bullet in red_bullets:
         pygame.draw.rect(WIN, RED, bullet)
-        
+
     for bullet in yellow_bullets:
         pygame.draw.rect(WIN, YELLOW, bullet)
 
     pygame.display.update()
+
+
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] and yellow.x - VEL > 0:  # LEFT
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] and yellow.x + VEL + yellow.width < BORDER.x:  # RIGHT
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_w] and yellow.y - VEL > 0:  # UP
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s] and yellow.y + VEL + yellow.height < HEIGHT - 15:  # DOWN
+        yellow.y += VEL
+
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_j] and red.x - VEL > 0:  # LEFT
+        red.x -= VEL
+    if keys_pressed[pygame.K_l] and red.x + VEL + red.width < BORDER.x:  # RIGHT
+        red.x -= VEL
+    if keys_pressed[pygame.K_i] and red.y - VEL > 0:  # UP
+        red.y -= VEL
+    if keys_pressed[pygame.K_k] and red.y + VEL + red.height < HEIGHT - 15:  # DOWN
+        red.y += VEL
 
 
 def main():
